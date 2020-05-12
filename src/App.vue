fd672f2d3ca1f259034bf3c22ed62ec653e07c62
kch2583 <kch2583@naver.com>
2020-05-12 22:09:20 +0900

    기초 설정, 로고 메뉴 만들기 시작

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <logoMenu></logoMenu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import logoMenu from "./components/defaultPage/logoMenu";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
+  components: {
+    logoMenu
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/defaultPage/logoMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="@/assets/daehyunLogo.png" alt="daehyun Logo" height="100px" />
+
+    <ul>
+      <li v-for="menuList in menuLists" :key="menuList.id">{{ menuList }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuLists: ["HOME", "PRODUCT", "CONTACT US"],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
