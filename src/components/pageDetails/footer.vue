01e2ab28cddfa95d905c7fd8d70e666cd52e384e
kch2583 <kch2583@naver.com>
2020-05-26 10:40:24 +0900

    footer font size setting

--- src/components/pageDetails/footer.vue
+++ src/components/pageDetails/footer.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="footer">
-    <v-footer padless dark>
-      <v-col>
-        <v-card>
-          <v-card-text>회사정보</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card>
-          <v-card-text>근무시간</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card>
-          <v-card-text>은행정보</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card>
-          <v-card-text>고객센터</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col class="text-center" cols="12">
-        {{ new Date().getFullYear() }} —
-        <strong>Vuetify</strong>
-      </v-col>
+    <v-footer padless>
+      <v-row>
+        <v-col cols="4" xs="6">
+          <v-card flat class="transparent">
+            <v-card-title>회사정보</v-card-title>
+            <v-card-text>hisoeifl fje oif oivkwfoi jsoijf eo</v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="4" xs="6">
+          <v-card flat class="transparent">
+            <v-card-title>은행정보</v-card-title>
+            <v-card-text>hisoeifl fje oif oivkwfoi jsoijf eo</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="4" xs="6">
+          <v-card flat class="transparent">
+            <v-card-title>고객센터</v-card-title>
+            <v-card-text>hisoeifl fje oif oivkwfoi jsoijf eo</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} —
+          <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
     </v-footer>
   </div>
 </template>
@@ -37,8 +38,48 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
-#footer {
-  background: pink;
+// <style lang="scss" scoped>
+.v-card__title {
+  font-size: 18px;
+}
+.v-card__text {
+  // font-size: 0.1rem;
+  font-size: 14px;
+}
+
+@media (min-width: 200px) {
+  .v-card__title {
+    font-size: 70% !important;
+  }
+  .v-card__text {
+    font-size: 70% !important;
+  }
+}
+
+@media (min-width: 500px) {
+  .v-card__title {
+    font-size: 80% !important;
+  }
+  .v-card__text {
+    font-size: 80% !important;
+  }
+}
+
+@media (min-width: 700px) {
+  .v-card__title {
+    font-size: 50% !important;
+  }
+  .v-card__text {
+    font-size: 50% !important;
+  }
+}
+
+@media (min-width: 1200px) {
+  .v-card__title {
+    font-size: 50% !important;
+  }
+  .v-card__text {
+    font-size: 50% !important;
+  }
 }
 </style>
